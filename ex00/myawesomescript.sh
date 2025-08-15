430bfd239c2fcd7472118095105e906042f32aee
blan <blandine.berthod@everdian.com>
2025-08-15 16:28:33 +0200

    possible optimization

--- ex00/myawesomescript.sh
+++ ex00/myawesomescript.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 curl -s -D - $1 | grep 'Location:' | cut -c 11-
+
+# optimisation possible : curl -s -D - "$1" | grep -i '^Location:' | cut -d ' ' -f 2
+# Sécuriser le cut en cas d’espaces multiples → cut -d ' ' -f 2
+# Cibler le header de manière insensible à la casse → grep -i '^Location:'
